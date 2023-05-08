91b4b093c5537670215b1a411769d65ce92d0c28
JWest11 <114183436+JWest11@users.noreply.github.com>
2023-05-08 13:46:52 -0600

    rasterizer functional

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <vector>
 #include <iostream>
+#include <thread>
 
 typedef float    f32;
 typedef double   f64;
@@ -60,17 +61,25 @@ class Vec3C {
         };
         Vec3C operator - (Vec3C& vec) {
             Vec3C output;
-            output.x = vec.x - x;
-            output.y = vec.y - y;
-            output.z = vec.z - z;
+            output.x = x - vec.x;
+            output.y = y - vec.y;
+            output.z = z - vec.z;
             return output;
         };
         Vec3C perspectiveDivide() {
             Vec3C output;
-            output.x = x / z;
-            output.y = y / z;
+            f32 positiveZ = z < 0 ? z * -1 : z;
+            positiveZ = positiveZ < 0.1 ? 0.1 : positiveZ;
+            output.x = x / positiveZ;
+            output.y = y / positiveZ;
             output.z = z;
             return output;
+        };
+        Vec3C crossProduct(Vec3C& vec) {
+            return Vec3C(z*vec.y-y*vec.z, x*vec.z-z*vec.x, y*vec.x-x*vec.y);
+        };
+        f32 magnitude() {
+            return sqrt(x*x + y*y + z*z);
         };
 };
 
@@ -83,10 +92,10 @@ class Vec3P {
         Vec3P() {
         };
         Vec3P(Vec3C& vec) {
-            r = sqrt(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
+            r = vec.magnitude();
             theta = acos(vec.y/r);
-            phi = acos(vec.x/sqrt(vec.x * vec.x + vec.z * vec.z));
-            if (vec.z < 0) {
+            phi = acos(vec.z/sqrt(vec.x * vec.x + vec.z * vec.z));
+            if (vec.x < 0) {
                 phi = PI * 2 - phi;
             };
         };
@@ -98,11 +107,14 @@ class Vec3P {
         Vec3C toVec3C() {
             Vec3C output;
             output.y = r * cos(theta);
-            output.z = r * sin(theta) * sin(phi);
-            output.x = r * sin(theta) * cos(phi);
+            output.x = r * sin(theta) * sin(phi);
+            output.z = r * sin(theta) * cos(phi);
             return output;
         };
     
+};
+void printVector(Vec3C& vec) {
+    std::cout << vec.x << " " << vec.y << " " << vec.z << std::endl;
 };
 
 f32 min3(f32 a, f32 b, f32 c) {
@@ -117,7 +129,11 @@ struct box2D {
     f32 x1;
     f32 y1;
 };
-
+struct BarycentricCoordinates {
+    f32 lambda0;
+    f32 lambda1;
+    f32 lambda2;
+};
 class ColMatrix3 {
     public:
         f32 data[9];
@@ -140,6 +156,7 @@ class ColMatrix3 {
             return output;
         };
 };
+
 class Camera {
     public:
         Vec3C position;
@@ -157,6 +174,7 @@ class Camera {
             Vec3P ZP = orientation;
             Vec3P XP = Vec3P(1, PI / 2, orientation.phi + PI / 2);
             Vec3P YP = Vec3P(1, orientation.theta - PI / 2, orientation.phi + PI);
+            
             Vec3C Z = ZP.toVec3C();
             Vec3C X = XP.toVec3C();
             Vec3C Y = YP.toVec3C();
@@ -165,6 +183,14 @@ class Camera {
             ColMatrix3 output = ColMatrix3(matrixValues);
             return output;
         };
+};
+
+Vec3C normalizeVecToCameraCanvas(Vec3C& vec, Camera& camera) {
+    Vec3C output;
+    output.x = (vec.x + camera.viewFrameRadiusX) / (2 * camera.viewFrameRadiusX);
+    output.y = (vec.y + camera.viewFrameRadiusY) / (2 * camera.viewFrameRadiusY);
+    output.z = vec.z;
+    return output;
 };
 class Triangle {
     public:
@@ -196,9 +222,9 @@ class Triangle {
         };
         Triangle toCameraSpace(ColMatrix3& cameraMatrix, Vec3C& cameraPosition) {
             Triangle output;
-            output.v0 = cameraMatrix * (v0 - cameraPosition) ;
-            output.v1 = cameraMatrix * (v1 - cameraPosition) ;
-            output.v2 = cameraMatrix * (v2 - cameraPosition) ;
+            output.v0 = cameraMatrix * (v0 - cameraPosition);
+            output.v1 = cameraMatrix * (v1 - cameraPosition);
+            output.v2 = cameraMatrix * (v2 - cameraPosition);
             return output;
             
         };
@@ -214,6 +240,53 @@ class Triangle {
             if (rect.x0 > camera.viewFrameRadiusX || rect.y0 > camera.viewFrameRadiusY || rect.x1 < camera.viewFrameRadiusX * -1 || rect.y1 < camera.viewFrameRadiusY * -1) {return false;};
             return true;
         };
+        bool pixelIsBounded(Vec3C& pixelRelative) {
+            if ((v1-v0).crossProduct(pixelRelative-v0).z > 0) {return false;};
+            if ((v2-v1).crossProduct(pixelRelative-v1).z > 0) {return false;};
+            if ((v0-v2).crossProduct(pixelRelative-v2).z > 0) {return false;};
+            return true;
+        };
+        Triangle normalizeToCameraCanvas(Camera& camera) {
+            Triangle output;
+            output.v0 = normalizeVecToCameraCanvas(v0, camera);
+            output.v1 = normalizeVecToCameraCanvas(v1, camera);
+            output.v2 = normalizeVecToCameraCanvas(v2, camera);
+            return output;
+        };
+        BarycentricCoordinates getBarycentricCoordinates(Vec3C& point) {
+            BarycentricCoordinates output;
+            f32 A0 = (v1-v2).crossProduct(point-v1).magnitude() / 2;
+            f32 A1 = (v2-v0).crossProduct(point-v2).magnitude() / 2;
+            f32 A2 = (v0-v1).crossProduct(point-v0).magnitude() / 2;
+            output.lambda0 = A0 / (A0 + A1 + A2);
+            output.lambda1 = A1 / (A0 + A1 + A2);
+            output.lambda2 = A2 / (A0 + A1 + A2);
+            return output;
+        };
+        Triangle rasterToCameraSpace(Camera& camera) {
+            Triangle output;
+            f32 zCamera = camera.position.z;
+            output.v0.x = v0.x * zCamera;
+            output.v0.y = v0.y * zCamera;
+            output.v0.z = v0.z;
+            output.v1.x = v1.x * zCamera;
+            output.v1.y = v1.y * zCamera;
+            output.v1.z = v1.z;
+            output.v2.x = v2.x * zCamera;
+            output.v2.y = v2.y * zCamera;
+            output.v2.z = v2.z;
+            return output;
+        };
+};
+
+struct VertexValues {
+    f32 val0;
+    f32 val1;
+    f32 val2;
+};
+
+f32 interpolate3D(BarycentricCoordinates barycentricCoordinates, VertexValues vertexValues) {
+    return barycentricCoordinates.lambda0 * vertexValues.val0 + barycentricCoordinates.lambda1 * vertexValues.val1 + barycentricCoordinates.lambda2 * vertexValues.val2;
 };
 
 class SDL {
@@ -222,23 +295,31 @@ class SDL {
         SDL_Renderer* renderer;
         SDL_Texture* texture;
 
+        Camera _camera;
+
         int pixelCountX;
         int pixelCountY;
         int pixelTotal;
         u32* pixels;
+        f32* pixelZBuffer;
 
-        SDL(int _pixelCountX, int _pixelCountY) {
+        const int zMax = 10;
+
+        SDL(int _pixelCountX, int _pixelCountY, int windowPixelsX, int windowPixelsY, Camera& cam) {
+            _camera = cam;
             pixelCountX = _pixelCountX;
             pixelCountY = _pixelCountY;
             pixelTotal = pixelCountX * pixelCountY;
             pixels = new u32[pixelTotal];
+            pixelZBuffer = new f32[pixelTotal];
             for (int i=0; i<pixelTotal; i++) {
                 pixels[i] = u32(0x000000FF);
+                pixelZBuffer[i] = f32(zMax);
             };
 
             SDL_Init(SDL_INIT_VIDEO);
 
-            window = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, pixelCountX, pixelCountY, NULL);
+            window = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowPixelsX, windowPixelsY, NULL);
             renderer = SDL_CreateRenderer(window, -1, NULL);
             texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, pixelCountX, pixelCountY);
             SDL_UpdateTexture(texture, NULL, pixels, pixelCountX * 4);
@@ -247,22 +328,74 @@ class SDL {
             SDL_RenderPresent(renderer);
         };
 
-        void renderRasterizedTriangle(Triangle& triangle) {
+        void reRenderNormalizedRasterizedTriangles(std::vector<Triangle>& triangles) {
+            for (Triangle& triangle : triangles) {
+                renderNormalizedRasterizedTriangle(triangle);
+            };
             for (int i=0; i<pixelTotal; i++) {
-                int pointX = (i % pixelCountX) / pixelCountX;
-                int pointY = (i / pixelCountX) / pixelCountY;
-                bool bounded = false;
-                if (triangle.v0)
-            }
+                pixelZBuffer[i] = f32(zMax);
+            };
+        };
+
+        void renderRow(int minX, int maxX, int minY, int maxY, Triangle triangle) {
+            for (int pixelY=minY; pixelY<maxY; pixelY++) {
+                for (int pixelX=minX; pixelX<maxX; pixelX++) {
+                    f32 relativeX = f32(pixelX) / f32(pixelCountX);
+                    f32 relativeY = f32(pixelY) / f32(pixelCountY);
+                    Vec3C relativePixel = Vec3C(relativeX, relativeY, 1);
+
+                    f32 zPixel = interpolate3D(triangle.getBarycentricCoordinates(relativePixel), {triangle.v0.z, triangle.v1.z, triangle.v2.z});
+
+                    int pixelIndex = pixelY * pixelCountX + pixelX;
+
+                    if (zPixel > pixelZBuffer[pixelIndex]) { continue; };
+
+                    if (zPixel > 0.2 && triangle.pixelIsBounded(relativePixel)) {
+                        u32 inputVal = 0x0000FFFF;
+                        pixels[pixelIndex] = inputVal;
+                        pixelZBuffer[pixelIndex] = zPixel;
+                    };
+                };
+            };
+        };
+
+        
+        void renderNormalizedRasterizedTriangle(Triangle& triangle) {
+            box2D boundingRect = triangle.getBoundingRectXY();
+            int minX = boundingRect.x0 * pixelCountX >= 0 ? boundingRect.x0 * pixelCountX : 0;
+            int minY = boundingRect.y0 * pixelCountY >= 0 ? boundingRect.y0 * pixelCountY : 0;
+            int maxX = boundingRect.x1 * pixelCountX <= pixelCountX ? boundingRect.x1 * pixelCountX : pixelCountX;
+            int maxY = boundingRect.y1 * pixelCountY <= pixelCountY ? boundingRect.y1 * pixelCountY : pixelCountY;
+
+            std::thread t1 = std::thread(&SDL::renderRow, this, minX, maxX, minY, maxY/8, triangle);
+            std::thread t2 = std::thread(&SDL::renderRow, this, minX, maxX, maxY/8, maxY/4, triangle);
+            std::thread t3 = std::thread(&SDL::renderRow, this, minX, maxX, maxY/4, maxY*3/8, triangle);
+            std::thread t4 = std::thread(&SDL::renderRow, this, minX, maxX, maxY*3/8, maxY/2, triangle);
+            std::thread t5 = std::thread(&SDL::renderRow, this, minX, maxX, maxY/2, maxY*5/8, triangle);
+            std::thread t6 = std::thread(&SDL::renderRow, this, minX, maxX, maxY*5/8, maxY*3/4, triangle);
+            std::thread t7 = std::thread(&SDL::renderRow, this, minX, maxX, maxY*3/4, maxY*7/8, triangle);
+            std::thread t8 = std::thread(&SDL::renderRow, this, minX, maxX, maxY*7/8, maxY, triangle);
+
+            t1.join();
+            t2.join();
+            t3.join();
+            t4.join();
+            t5.join();
+            t6.join();
+            t7.join();
+            t8.join();
             
         };
 
         void render() {
+            SDL_RenderClear(renderer);
             SDL_UpdateTexture(texture, NULL, pixels, pixelCountX * 4);
-            SDL_RenderCopy(renderer, texture, NULL, NULL);
+            SDL_RenderCopyEx(renderer, texture, NULL, NULL, 0.0, NULL, SDL_FLIP_VERTICAL);
+            SDL_RenderPresent(renderer);
         };
 
         ~SDL() {
+            delete[] pixelZBuffer;
             delete[] pixels;
             SDL_DestroyTexture(texture);
             SDL_DestroyRenderer(renderer);
@@ -271,34 +404,39 @@ class SDL {
         };
 };
 
+
+void printTriangle(Triangle& triangle) {
+    std::cout << "v0:" << " ";
+    printVector(triangle.v0);
+    std::cout << "v1:" << " ";
+    printVector(triangle.v1);
+    std::cout << "v2:" << " ";
+    printVector(triangle.v2);
+};
+
+void printCamera(Camera& camera) {
+    std::cout << "position:";
+    printVector(camera.position);
+    std::cout << "orientation:";
+    std::cout << "theta: " << camera.orientation.theta/PI << " " << "phi: " << camera.orientation.phi/PI << std::endl;
+};
+
+
 int main(int argc, char *argv[]) {
 
-    SDL SDLState = SDL(1024, 768);
+    
 
     Camera camera = Camera();
+
+    SDL SDLState = SDL(800, 600, 1600, 1200, camera);
 
     std::vector<Triangle> trianglesWorldSpace;
     std::vector<Triangle> trianglesCameraSpace;
     std::vector<Triangle> trianglesRasterSpace;
-    Triangle input = Triangle(Vec3C(0,0,1),Vec3C(1,1,1),Vec3C(0,1,1));
+
+    Triangle input = Triangle(Vec3C(0,1,4),Vec3C(0,0,4),Vec3C(1,0,4));
     trianglesWorldSpace.push_back(input);
 
-    ColMatrix3 cameraMatrix = camera.createCameraMatrix();
-
-    for (Triangle triangle : trianglesWorldSpace) {
-        trianglesCameraSpace.push_back(triangle.toCameraSpace(cameraMatrix, camera.position));
-    };
-
-    for (Triangle triangle : trianglesCameraSpace) {
-        Triangle rasterTriangle = triangle.toRasterSpace();
-        if (rasterTriangle.rasterTriangleIsVisible(camera)) {
-            trianglesRasterSpace.push_back(rasterTriangle);
-        };
-    };
-
-    for (Triangle triangle : trianglesRasterSpace) {
-        SDLState.renderRasterizedTriangle();
-    };
 
     SDL_Event e;
     bool quit = false;
@@ -307,12 +445,63 @@ int main(int argc, char *argv[]) {
             if (e.type == SDL_QUIT){
                 quit = true;
             }
-            if (e.type == SDL_KEYDOWN){
-                quit = true;
+            if (e.type == SDL_KEYDOWN) {
+                if (e.key.keysym.sym == SDLK_w) {
+                    camera.position = camera.position + Vec3C(0,0,0.1);
+                }
+                if (e.key.keysym.sym == SDLK_s) {
+                    camera.position = camera.position - Vec3C(0,0,0.1);
+                }
+                if (e.key.keysym.sym == SDLK_a) {
+                    camera.position = camera.position - Vec3C(0.1,0,0);
+                }
+                if (e.key.keysym.sym == SDLK_d) {
+                    camera.position = camera.position + Vec3C(0.1,0,0);
+                }
+                if (e.key.keysym.sym == SDLK_LEFT) {
+                    camera.orientation.phi += 0.05;
+                }
+                if (e.key.keysym.sym == SDLK_RIGHT) {
+                    camera.orientation.phi -= 0.05;
+                }
+                if (e.key.keysym.sym == SDLK_UP) {
+                    camera.orientation.theta += 0.05;
+                }
+                if (e.key.keysym.sym == SDLK_DOWN) {
+                    camera.orientation.theta -= 0.05;
+                }
             }
-            if (e.type == SDL_MOUSEBUTTONDOWN){
-                quit = true;
-            }
+
+            for (int i=0; i<SDLState.pixelTotal; i++) {
+                SDLState.pixels[i] = 0x000000FF;
+            };
+
+            trianglesCameraSpace.clear();
+            trianglesRasterSpace.clear();
+
+            ColMatrix3 cameraMatrix = camera.createCameraMatrix();
+
+            for (Triangle triangle : trianglesWorldSpace) {
+                Triangle triangleCameraSpace = triangle.toCameraSpace(cameraMatrix, camera.position);
+                trianglesCameraSpace.push_back(triangleCameraSpace);
+                std::cout << "camera: ";
+                printTriangle(triangleCameraSpace);
+                printCamera(camera);
+            };
+
+            for (Triangle triangle : trianglesCameraSpace) {
+                Triangle rasterTriangle = triangle.toRasterSpace();
+                std::cout << "raster: ";
+                printTriangle(rasterTriangle);
+                if (rasterTriangle.rasterTriangleIsVisible(camera)) {
+                    Triangle normalizedRasterizedTriangle = rasterTriangle.normalizeToCameraCanvas(camera);
+                    trianglesRasterSpace.push_back(normalizedRasterizedTriangle);
+                };
+            };
+
+            SDLState.reRenderNormalizedRasterizedTriangles(trianglesRasterSpace);
+
+            SDLState.render();
         }
     }
 
